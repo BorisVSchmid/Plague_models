c15fa2008746b227ff6f508244085543b9dfee40
rhjvedder <rolf.vedder@gmail.com>
2017-09-21 16:05:54 +0200

    index out of bounds error for the main loop, is strange, will figure it out tomorrow

--- models/factors.py
+++ models/factors.py
@@ -1,13 +1,23 @@
 import numpy as np
+import pandas as pd
+from datetime import datetime
 import matplotlib.pyplot as plt
 
 
 def run():
+    span = [time.strftime("%Y-%m-%d") for time in pd.date_range(datetime(1990, 1, 1), datetime(2000, 12, 31))]
+    print(span)
     # -- Params
-    t = [i for i in range(1001)]
+    temp = [[31.1, 27.1, 23.6, 82], [30.8, 27.2, 23.8, 83], [32.5, 27.5, 23.6, 81], [32.5, 27.4, 22.9, 73],
+         [32.0, 26.1, 20.6, 67], [31.0, 24.6, 18.6, 64], [30.8, 24.2, 18.0, 62], [31.4, 24.6, 18.4, 60],
+         [32.1, 25.4, 19.6, 63], [32.5, 26.8, 22.0, 66], [32.2, 27.7, 23.5, 72], [31.3, 27.4, 23.7, 80]]
+    decade = {}
+    for time in span:
+        decade[time] = temp[int(time.split("-")[1])-1]
+    t = [x for x in range(1, len(decade)-1)]
     # - Human
     beta_h = .2
-    s_h = 100000.
+    s_h = 25000.
     i_h = np.zeros_like(t)
     r_h = np.zeros_like(t)
     d_h = np.zeros_like(t)
@@ -18,27 +28,29 @@ def run():
     i_r = np.zeros_like(t)
     rec_r = np.zeros_like(t)
     d_r = np.zeros_like(t)
-    sus_frac = .08
+    sus_frac = .1
+    # 0.08
     s_r[0] = s_h * sus_frac
     beta_r = .08
-    i_r[0] = 15.
+    i_r[0] = 0.
     gamma_r = 0.2
     por_r = .1
-    rep_rate = .2
+    rep_rate_r = .5
+    rep_rate_ur = .4
     inh_res = 0.975
     # - flea
     d_rate = 0.2
+    # 0.2
     g_rate = .0084
     c_cap = 6.
     i_f = np.zeros_like(t)
     fph = np.zeros_like(t)
     fph[0] = c_cap
     searching = 3. / s_r[0]
-    week = 0
     # -- Simulate
-    for i in t[1:]:
+    for i in t:
         N_r = s_r[i - 1] + i_r[i - 1] + rec_r[i - 1]
-        # Fleas
+        # - Fleas
         if i == 1:
             infected_rat_deaths = d_h[0]
             c_cap = fph[0]  # avg number of fleas per rat at carrying capacity
@@ -48,36 +60,45 @@ def run():
             flea_growth = -g_rate * (fph[i - 1] * (1. - (fph[i - 1] / c_cap)))
         else:
             flea_growth = 0.
-
         new_infectious = infected_rat_deaths * fph[i - 1]
         starvation_deaths = d_rate * i_f[i - 1]
         force_to_humans = min(i_f[i - 1], i_f[i - 1] * np.exp(-searching * N_r))  # number of fleas that find a human
         force_to_rats = i_f[i - 1] - force_to_humans  # number of fleas that find a rat
         fph[i] = fph[i - 1] + flea_growth
         i_f[i] = i_f[i - 1] + new_infectious - starvation_deaths
-        # Rats
+        # - Rats
         new_infected_rats = min(s_r[i - 1], beta_r * s_r[i - 1] * force_to_rats / N_r)
         new_removed_rats = gamma_r * i_r[i - 1]
         new_recovered_rats = por_r * new_removed_rats
         new_dead_rats = new_removed_rats - new_recovered_rats
         infected_rat_deaths = new_dead_rats
-        born_rats = (rep_rate * s_r[i - 1] * (1. - (N_r/c_cap))) + (rep_rate * rec_r[i - 1] * (1. - inh_res))
-        s_r[i] = (rep_rate * s_r[i - 1]) - new_infected_rats - (s_r[i - 1] * d_rate) + born_rats
+        # born rats
+        resistant_born_rats = (rep_rate_r * rec_r[i - 1] * (inh_res - (N_r / s_h)))
+        resistant_born_rats = 0 if N_r / s_h < 0 else resistant_born_rats
+        unresistant_from_resistant = (rep_rate_ur * rec_r[i - 1] * (1. - inh_res))
+        unresistant_born_rats = (rep_rate_ur * s_r[i - 1] * (1. - (N_r / s_h)))
+        unresistant_born_rats = 0 if N_r / s_h < 0 else unresistant_born_rats
+        born_rats = unresistant_born_rats + unresistant_from_resistant
+        # natural deaths
+        natural_death_unresistant = (s_r[i - 1] * d_rate)
+        natural_death_resistant = (rec_r[i - 1] * d_rate)
+        # time step values
+        s_r[i] = s_r[i - 1] + born_rats - new_infected_rats - natural_death_unresistant
         i_r[i] = i_r[i - 1] + new_infected_rats - new_removed_rats
-        rec_r[i] = rec_r[i - 1] + new_recovered_rats + (rep_rate * rec_r[i - 1] * (inh_res - (N_r/c_cap))) - (d_rate * rec_r[i - 1])
-        d_r[i] = new_dead_rats
-        # Humans
+        rec_r[i] = rec_r[i - 1] + new_recovered_rats + resistant_born_rats - natural_death_resistant
+        d_r[i] = new_dead_rats + natural_death_unresistant + natural_death_resistant
+        # - Humans
         N_h = s_h + i_h[i - 1] + r_h[i - 1]
         new_infected_humans = min(s_h, beta_h * s_h * force_to_humans / N_h)
         new_removed_humans = gamma_h * i_h[i - 1]
         new_recovered_humans = por_h * new_removed_humans
         new_dead_humans = new_removed_humans - new_recovered_humans
+        # time step values
         i_h[i] = i_h[i - 1] + new_infected_humans - new_removed_humans
         r_h[i] = r_h[i - 1] + new_recovered_humans
         d_h[i] = new_dead_humans
-        if i % 7 == 0 and i != 0:
-            week += 1
-            print("{} dead humans in week{} and {} dead rats".format(str(sum(d_h[i-7:i])), week, str(sum(d_r[i-7:i]))))
+    years = [x for x in range(1990, 2000)]
+    plt.xticks(years)
     plt.style.use('ggplot')
     plt.rc('font', size=16)
     plt.rc('lines', linewidth=2)
@@ -87,6 +108,7 @@ def run():
     plt.plot(d_r, label='dead_rats')
     plt.plot(s_r, label='rats')
     plt.plot(i_r, label="infected rats")
+    plt.plot(rec_r, label="resistant rats")
     plt.legend(loc='best')
     # plt.savefig('SIRD_model.png')
     plt.show()
